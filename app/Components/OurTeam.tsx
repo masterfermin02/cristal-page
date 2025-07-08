65140b8035373e8048751d7094ab35cf3cef9f4d
Fermin <masterfermin02@gmail.com>
2025-07-08 17:56:57 -0400

    Added content to the page

--- app/Components/OurTeam.tsx
+++ app/Components/OurTeam.tsx
@@ -21,7 +21,7 @@ const TeamSection = forwardRef(({ seePlanCristalClick, ref }: OurTeamProps) => {
                 </div>
                 <div className="row">
                     <div className="col-lg-4 col-md-6 col-sm-12">
-                        <div className="card shadow-sm">
+                        <div className="card h-100 shadow-sm">
                             <div style={{ position: 'relative', width: '100%', height: '325px' }}>
                                 <Image
                                     src="/../assets/img/team/cristal.jpeg"
@@ -31,9 +31,9 @@ const TeamSection = forwardRef(({ seePlanCristalClick, ref }: OurTeamProps) => {
                                     objectPosition='top'
                                     />
                             </div>
-                            <div className="card-body">
+                            <div className="card-body d-flex flex-column">
                                 <h2>Cristal Lopez</h2>
-                                <p className="card-text">
+                                <div className="card-text flex-grow-1">
                                     <ul>
                                         <li>Entrenadora personal certificada (FDFF)</li>
                                         <li>Certificación nutrición y dietética aplicada al deporte (FDFF)</li> 
@@ -42,8 +42,8 @@ const TeamSection = forwardRef(({ seePlanCristalClick, ref }: OurTeamProps) => {
                                         <li>Taller suplementarios y farmacología</li>
                                         <li>Taller peak week management</li>
                                     </ul>
-                                </p>
-                                <div className="d-flex justify-content-between align-items-center">
+                                </div>
+                                <div className="mt-3">
                                     <div className="btn-group">
                                         <button type='button' onClick={seePlanCristalClick} className="btn btn-sm btn-outline-secondary">Ver planes</button>
                                     </div>
@@ -52,7 +52,7 @@ const TeamSection = forwardRef(({ seePlanCristalClick, ref }: OurTeamProps) => {
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12 mt-4">
-                        <div className="card shadow-sm">
+                        <div className="card h-100 shadow-sm">
                             <div style={{ position: 'relative', width: '100%', height: '325px' }}>
                                 <Image
                                     src="/../assets/img/team/yoedy.jpeg"
@@ -62,16 +62,16 @@ const TeamSection = forwardRef(({ seePlanCristalClick, ref }: OurTeamProps) => {
                                     objectPosition='top'
                                 />
                             </div>
-                            <div className="card-body">
+                            <div className="card-body d-flex flex-column">
                                 <h2>Yoedy Bernard</h2>
-                                <p className="card-text">
+                                <div className="card-text flex-grow-1">
                                     <ul>
                                         <li>- Coach internacional en competención de culturismo y fitness ( ifbb Academy )</li>
                                         <li>- Certificado en nutrición, ejercicios y deportes (Wageningen  university and research )</li>
                                         <li>- Estudiante de maestría en nutrición  (Escuela de Postgrado de Medicina y Sanidad, grupo esceneca)</li>
                                     </ul>
-                                </p>
-                                <div className="d-flex justify-content-between align-items-center">
+                                </div>
+                                <div className="mt-3">
                                     <div className="btn-group">
                                     <a href={`https://wa.me/+18493766655?text=${DEFAULT_WHATSAPP_MESSAGE}`} className="btn btn-sm btn-outline-secondary">Ver planes</a>
                                     </div>
@@ -80,7 +80,7 @@ const TeamSection = forwardRef(({ seePlanCristalClick, ref }: OurTeamProps) => {
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12 mt-4">
-                        <div className="card shadow-sm">
+                        <div className="card h-100 shadow-sm">
                             <div style={{ position: 'relative', width: '100%', height: '325px' }}>
                                 <Image
                                         src="/../assets/img/team/joan-2.jpeg"
@@ -90,16 +90,16 @@ const TeamSection = forwardRef(({ seePlanCristalClick, ref }: OurTeamProps) => {
                                         objectPosition='top'
                                     />
                             </div>
-                            <div className="card-body">
+                            <div className="card-body d-flex flex-column">
                                 <h2>Joan Guzman</h2>
-                                <p className="card-text">
+                                <div className="card-text flex-grow-1">
                                     <ul>
                                         <li>- MLB player performance specialist</li>
                                         <li>- Sport nutrítion</li>
                                         <li>- FMS level 1</li>
                                     </ul>
-                                </p>
-                                <div className="d-flex justify-content-between align-items-center">
+                                </div>
+                                <div className="mt-3">
                                     <div className="btn-group">
                                         <a href={`https://wa.me/+18297803248?text=${DEFAULT_WHATSAPP_MESSAGE}`} className="btn btn-sm btn-outline-secondary">Ver planes</a>
                                     </div>
@@ -108,29 +108,30 @@ const TeamSection = forwardRef(({ seePlanCristalClick, ref }: OurTeamProps) => {
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12 mt-4">
-                        <div className="card shadow-sm">
-                            <div style={{ position: 'relative', width: '100%', height: '325px' }}>
+                        <div className="card h-100 shadow-sm">
+                            <div style={{ position: 'relative', width: '100%', height: '325px', backgroundColor: 'black' }}>
                                 <Image
-                                        src="/../assets/img/team/elvissaul.jpeg"
-                                        alt="Saul coach"
+                                        src="/../assets/img/team/daniel.png"
+                                        alt="Daniel coach"
                                         layout='fill'
                                         objectFit='cover'
                                          objectPosition='top'
                                     />
                             </div>
-                            <div className="card-body">
-                                <h2>Elvisaul Núñez</h2>
-                                <p className="card-text">
+                            <div className="card-body d-flex flex-column">
+                                <h2>Daniel Roca</h2>
+                                <div className="card-text flex-grow-1">
                                     <ul>
-                                        <li>IFBB International Coach</li>
-                                        <li>Certification en nutrición deportiva</li>
-                                        <li>Certificación en masajes deportivos y relajantes</li>
-                                        <li>Certificación en primeros auxilios</li>
+                                        <li>- Licenciado en nutrición y dietética </li>
+                                        <li>- Entrenador personal certificado (Mega)</li>
+                                        <li>- Certificación en entrenamiento funcional (Mega)</li>
+                                        <li>- Certificación en nutrición deportiva (cienut)</li>
+                                        <li>- Cursos de antropometría (isak)</li>
                                     </ul>
-                                </p>
-                                <div className="d-flex justify-content-between align-items-center">
+                                </div>
+                                <div className="mt-3">
                                     <div className="btn-group">
-                                        <a href={`https://wa.me/+18493516061?text=${DEFAULT_WHATSAPP_MESSAGE}`} className="btn btn-sm btn-outline-secondary">Ver planes</a>
+                                        <a href={`https://wa.me/+18092815102?text=${DEFAULT_WHATSAPP_MESSAGE}`} className="btn btn-sm btn-outline-secondary">Ver planes</a>
                                     </div>
                                 </div>
                             </div>
