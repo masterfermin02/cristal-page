8064256817d6c015e84834ae85e6f1f7448c2201
Fermin <masterfermin02@gmail.com>
2026-01-06 21:43:12 -0400

    Refactor service section layout for better responsiveness and adjust container width

--- app/Components/ServiceSection.tsx
+++ app/Components/ServiceSection.tsx
@@ -3,7 +3,7 @@ import SingleFeature from "@/app/Components/SingleFeature";
 
 const ServicesSection = () => {
     return (
-        <section id={'planes'} className="pricing-area section-padding40 fix">
+        <section id={'planes'} className="pricing-area section-padding40">
             <div className="container">
                 <div className="row">
                     <div className="col-xl-12">
@@ -12,11 +12,11 @@ const ServicesSection = () => {
                         </div>
                     </div>
                 </div>
-                <div className={'row'}>
-                    <div className={'col-lg-3 col-md-3 col-sm-12'}>
-                        <div className="properties mb-30 wow fadeInUp">
-                            <div className="properties__card">
-                                <div className="properties__caption">
+                <div className="row">
+                    <div className="col-12 col-md-6 col-lg-3 d-flex">
+                        <div className="properties mb-30 wow fadeInUp w-100">
+                            <div className="properties__card h-100 d-flex flex-column">
+                                <div className="properties__caption flex-grow-1">
                                     <SingleFeature label="CLASE DE PRUEBA - GRATIS" />
                                     <SingleFeature label="CLASE SIN INSCRIPCION - RD$1,000" />
                                     <SingleFeature label="PROGRAMA DE ENTRENAMIENTO ADAPATADO A SU CONDICION FISICA Y SU OBJETIVO (INCLUYE ACCESO A LA APP DE SEGUIMIENTO) - RD$3,500" />
@@ -24,10 +24,10 @@ const ServicesSection = () => {
                             </div>
                         </div>
                     </div>
-                    <div className={'col-lg-3 col-md-3 col-sm-12'}>
-                        <div className="properties mb-30 wow fadeInUp">
-                            <div className="properties__card">
-                                <div className="properties__caption">
+                    <div className="col-12 col-md-6 col-lg-3 d-flex">
+                        <div className="properties mb-30 wow fadeInUp w-100">
+                            <div className="properties__card h-100 d-flex flex-column">
+                                <div className="properties__caption flex-grow-1">
                                     <SingleFeature label="PLAN DE NUTRICION PERSONALIZADO - RD$3,500" />
                                     <SingleFeature label="EVALUACION DE SEGUIMIENTO INBODY - RD$500" />
                                     <SingleFeature label="REPOSICION/CAMBIO DE HORARIO - RD$500" />
@@ -35,20 +35,20 @@ const ServicesSection = () => {
                             </div>
                         </div>
                     </div>
-                    <div className={'col-lg-3 col-md-3 col-sm-12'}>
-                        <div className="properties mb-30 wow fadeInUp">
-                            <div className="properties__card">
-                                <div className="properties__caption">
+                    <div className="col-12 col-md-6 col-lg-3 d-flex">
+                        <div className="properties mb-30 wow fadeInUp w-100">
+                            <div className="properties__card h-100 d-flex flex-column">
+                                <div className="properties__caption flex-grow-1">
                                     <SingleFeature label="MASAJE DE DESCARGA MUSCULAR - RD$2,000" />
                                     <SingleFeature label="TERAPIA DE CUPPING/VENTOSAS - RD$2,000" />
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className={'col-lg-3 col-md-3 col-sm-12'}>
-                        <div className="properties mb-30 wow fadeInUp">
-                            <div className="properties__card">
-                                <div className="properties__caption">
+                    <div className="col-12 col-md-6 col-lg-3 d-flex">
+                        <div className="properties mb-30 wow fadeInUp w-100">
+                            <div className="properties__card h-100 d-flex flex-column">
+                                <div className="properties__caption flex-grow-1">
                                     <SingleFeature label="READAPTACION DEPORTIVA - RD$2,000" />
                                     <SingleFeature label="TERAPIA VIBRATORIA - RD$2,000" />
                                 </div>
